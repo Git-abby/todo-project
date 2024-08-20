7fe409b1a9baebbb434db0fb7affb62df9c03c26
Git-abby <malekabrar56@gmail.com>
2024-08-19 20:07:20 -0400

    Task Completed: Style added to heading

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Heading() {
   return (
-    <h1 className='text-4xl font-semibold text-slate-800 capitalize'>What's your list for today...</h1>
+    // <h1 className='text-4xl font-semibold text-slate-800 capitalize'>What's your list for today...</h1>
+    <h1 className='text-4xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-slate-400 via-slate-500 to-slate-600 shadow-lg'>
+    What's your list for today...
+  </h1>
+  
   )
 }
 
